0f12987775f48e486e094613eb0a6e27c99caca5
七彩枫叶 <424235748@qq.com>
2023-12-29 23:58:43 +0800

    创建captcha

--- app/common/service/CaptchaService.php
+++ app/common/service/CaptchaService.php
@@ -1,9 +1,38 @@
 <?php declare (strict_types=1);
-/**
- * 验证码生成
-*/
+
 namespace app\common\service;
 class CaptchaService
 {
-    
+    /**
+     * 生成验证码
+    */
+    public function create()
+    {
+        // 防止外部调用验证码图片
+        $form = $_SERVER['HTTP_REFERER'];
+        $host = $_SERVER['HTTP_HOST'];
+
+        if ($form && strpos($form, '://' . $host) != 4 && strpos($form, '://' . $host) != 5) {
+            die('非法调用验证码！');
+        }
+
+// 记录验证码
+        session_start(); // 启动会话
+
+// 初始化验证码
+        $code = new Code();
+        $code->height = 45;
+        $code->width = 120;
+        $code->fontsize = 18;
+        $code->charset = 'abcdefghkmnprtuvwxy23456789ABCDEFGHKMNPRTUVWXY';
+        $code->doimg();
+        session('checkcode', $code->getCode());
+    }
+    /**
+     * 检验验证码
+    */
+    public function check()
+    {
+
+    }
 }
