f86b51db684c43229a08880a24ed37b67928bb3f
minsishuju <424235748@qq.com>
2024-01-05 14:46:56 +0800

    短连接 占位

---
+++ app/common/library/ShortLink.php
@@ -0,0 +1,16 @@
+<?php declare (strict_types=1);
+/*
+ * TomatoAdmin 基础开发框架[tomatosaas]
+ * ShortLink create at 2024/1/5 14:27
+ * Author: 七彩枫叶 Email：424235748@qq.com
+*/
+
+namespace app\common\library;
+
+/**
+ * 短连接管理
+*/
+class ShortLink
+{
+
+}
