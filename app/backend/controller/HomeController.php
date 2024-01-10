32a72d08a42adb65a5734c3f7182d2efcaa975c7
minsishuju <424235748@qq.com>
2024-01-10 18:25:32 +0800

    新增curl库

---
+++ app/backend/controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php declare (strict_types=1);
+/*
+ * TomatoAdmin 基础开发框架[tomatosaas]
+ * HomeController create at 2024/1/10 11:09
+ * Author: 七彩枫叶 Email：424235748@qq.com
+*/
+
+namespace app\backend\controller;
+
+use tomato\request\Curl;
+class HomeController
+{
+    public function index()
+    {
+        $http = new Curl();
+        $http->url();
+    }
+}
---
+++ extend/tomato/request/Curl.php
@@ -0,0 +1,52 @@
+<?php declare (strict_types=1);
+namespace tomato\request;
+
+class Curl
+{
+    private $ch;
+    private string $base_url;
+    //构造函数
+    public function __construct($base_url)
+    {
+//        if(!function_exists('curl_init'))
+//        {
+//            die('正在使用的php版本，不支持Curl函数！');
+//        }
+        $this->base_url = $base_url;
+        $this->ch       = curl_init();
+    }
+
+    /**
+     * 请求方式 建议使用post get 其它方法服务器端不一定支持
+     * @param string $method 请求方式 默认get
+    */
+    public function HttpMethod(string $method='get')
+    {
+        match ($method)
+        {
+            'get'    => $this->ch,
+            'post'   => curl_setopt($this->ch, CURLOPT_POST, true),
+            'put'    => curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "PUT"),
+            'delete' => curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "DELETE"),
+        };
+    }
+    /**
+     * 设置具体请求的路径
+     * @param string $path
+    */
+    public function setUri(string $path)
+    {
+        curl_setopt ($this->ch, CURLOPT_URL, $this->base_url.$path);
+    }
+
+
+
+
+
+
+    //析构函数
+    public function __destruct()
+    {
+        curl_close($this->ch);
+    }
+}
